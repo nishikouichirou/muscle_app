2e08a527a899a413b8bb50aeea2004aa2b224714
EC2 Default User <ec2-user@ip-172-31-31-20.ap-northeast-1.compute.internal>
2021-05-06 12:01:55 +0000

    ユーザーの編集第10章

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,10 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+  
+  def current_user?(user)
+    user == current_user
+  end
 
   def current_user
     if (user_id = session[:user_id])
@@ -19,4 +23,16 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+  
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+  
 end
