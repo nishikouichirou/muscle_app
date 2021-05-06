264aac13b0d5da37c5598bf1a7980128a518c399
EC2 Default User <ec2-user@ip-172-31-31-20.ap-northeast-1.compute.internal>
2021-05-06 07:10:13 +0000

    第7章ユーザー登録

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
-
-  # => contact_path
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #root 'application#hello'
-
+  post '/signup',  to: 'users#create'
+  resources :users
 end
