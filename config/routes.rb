d6a25a73bcb7eabbfb87b824ed2c3d07bbfc4f44
EC2 Default User <ec2-user@ip-172-31-31-20.ap-northeast-1.compute.internal>
2021-05-07 09:57:40 +0000

    routes編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
-  get 'instructors/new'
+  
 
-  get 'users/new'
-
-  get 'sessions/new'
-
-  root 'static_pages#home' # => root_path
+  root 'static_pages#home'
+  
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
 
+  get  '/instructors_signup',  to: 'instructors#new'
+  post '/instructors_signup',  to: 'instructors#create'
+
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
+  
   delete '/logout',  to: 'sessions#destroy'
-
+  delete '/instructors_logout',  to: 'sessions#instructor_destroy'
+  
   resources :users
+  resources :instructors
 end
