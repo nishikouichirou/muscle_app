dbc21b92bdd96582cb0fe9ce3d53ce43a176a5dd
EC2 Default User <ec2-user@ip-172-31-31-20.ap-northeast-1.compute.internal>
2021-05-07 09:38:34 +0000

    セッションコントローラー作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,37 @@
 class SessionsController < ApplicationController
 
-  # GET /login
   def new
-    #@session = Session.new
   end
 
-  # POST /login
   def create
     user = User.find_by(email: params[:session][:email])
+    instructor = Instructor.find_by(email: params[:session][:email])
+    
     if user && user.authenticate(params[:session][:password])
-      # Success
       log_in user
-      redirect_to user
-    else
-      # Failure
-      flash.now[:danger] = 'Invalid email/password combination'
+      redirect_to root_path
+    elsif user && !user.authenticate(params[:session][:password])
+      flash.now[:danger] = 'メールアドレスとパスワードが一致していません'
+      render 'new'
+    elsif instructor && instructor.authenticate(params[:session][:password])
+      instructors_log_in instructor
+      redirect_to root_path
+    elsif instructor && !instructor.authenticate(params[:session][:password])
+      flash.now[:danger] = 'メールアドレスとパスワードが一致していません'
+      render 'new'
+    elsif user.nil? && instructor.nil?
+      flash.now[:danger] = 'メールアドレスとパスワードが一致していません'
       render 'new'
     end
   end
 
-  # DELETE /logout
   def destroy
     log_out if logged_in?
+    redirect_to root_url
+  end
+  
+  def instructor_destroy
+    instructors_log_out if instructors_logged_in?
     redirect_to root_url
   end
 end
