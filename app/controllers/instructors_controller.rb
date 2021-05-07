b69510aa823af15db66c0c3ec51c264789486a54
EC2 Default User <ec2-user@ip-172-31-31-20.ap-northeast-1.compute.internal>
2021-05-07 09:25:46 +0000

    インストラクターコントローラー作成

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,4 +1,93 @@
 class InstructorsController < ApplicationController
-  def new
+  before_action :logged_in_instructor, only: [:index, :edit,
+                                        :update, :destroy]
+  before_action :correct_instructor,   only: [:edit, :update]
+  before_action :admin_instructor,     only: :destroy
+
+  # def index
+  #   #@users = User.all
+  #   @users = User.paginate(page: params[:page])
+  # end
+
+  # GET /users/:id
+  def show
+    @instructor = Instructor.find(params[:id])
+    # => app/views/users/show.html.erb
+    # debugger
   end
+
+  # GET /users/new
+  def new
+    @instructor = Instructor.new
+    # => form_for @user
+  end
+
+  # POST /users
+  def create
+    @instructor = Instructor.new(user_params)
+    if @instructor.save # => Validation
+      # Sucess
+      log_in @instructor
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @instructor
+      # GET "/users/#{@user.id}" => show
+    else
+      # Failure
+      render 'new'
+    end
+  end
+
+  # GET /users/:id/edit
+  # params[:id] => :id
+  def edit
+    @instructor = Instructor.find(params[:id])
+    # => app/views/users/edit.html.erb
+  end
+
+  #PATCH /users/:id
+  def update
+    @instructor = Instructor.find(params[:id])
+    if @instructor.update_attributes(instructor_params)
+      # Success
+      flash[:success] = "Profile updated"
+      redirect_to @instructor
+    else
+      # Failure
+      # => @user.errors.full_messages()
+      render 'edit'
+    end
+  end
+
+  # DELETE /users/:id
+  def destroy
+    Instructor.find(params[:id]).destroy
+    flash[:success] = "Instructor deleted"
+    redirect_to instructors_url
+  end
+
+  private
+
+    def instructor_params
+      params.require(:instructor).permit(
+        :name, :email, :password,
+        :password_confirmation)
+    end
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_instructor
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_instructor
+      # GET   /users/:id/edit
+      # PATCH /users/:id
+      @instructor = Instructor.find(params[:id])
+      redirect_to(root_url) unless current_instructor?(@instructor)
+    end
+
 end
