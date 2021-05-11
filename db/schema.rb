0ce22fbb0f844c8216bde7fb8f21cba90d6bfa70
EC2 Default User <ec2-user@ip-172-31-18-43.ap-northeast-1.compute.internal>
2021-05-11 10:25:25 +0000

    Userのimageカラム消去imgカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210511075145) do
+ActiveRecord::Schema.define(version: 20210511094129) do
 
   create_table "chatrooms", force: :cascade do |t|
     t.integer "user_id"
